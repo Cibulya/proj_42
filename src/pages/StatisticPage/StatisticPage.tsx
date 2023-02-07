97e3241c8356e768ac4f15f7076ae52985349975
DrobyshSV <austin@tut.by>
2023-02-07 17:33:40 +0300

    feat: add canvasClock components using digital fonts"

--- src/pages/StatisticPage/StatisticPage.tsx
+++ src/pages/StatisticPage/StatisticPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import CanvasClock from 'components/canvasClock';
 
 const StatisticPage = () => {
   return (
     <div>
-      
+      <CanvasClock id="canvas" top="5" left="-15" width="100" height="50" fontSize = "14"/>
     </div>
   );
 };
