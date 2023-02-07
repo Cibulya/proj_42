b6cb3c691bb7f78908ce6e42ed22fa34480c9c58
DrobyshSV <austin@tut.by>
2023-02-07 17:14:19 +0300

    feat: add svgr and file-loader to config

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,18 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+	const svgLoader = {
+		test: /\.svg$/,
+		use: ['@svgr/webpack'],
+	}
+	const fileLoader = {
+		test: /\.(png|jpe?g|gif|woff|woff2|ttf|eot)$/i,
+		use: [
+			{
+				loader: 'file-loader',
+			},
+		],
+	}
 	const cssLoader = {
 		test: /\.s[ac]ss$/i,
 		use: [
@@ -28,5 +40,9 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		use: 'ts-loader',
 		exclude: /node_modules/,
 	};
-	return [typescriptLoader, cssLoader];
+	return [
+		fileLoader,
+		svgLoader,
+		typescriptLoader,
+		cssLoader];
 }
