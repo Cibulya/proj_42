a9ab58e753d03317996cb257c31d937fd505d589
DrobyshSV <austin@tut.by>
2023-02-08 21:59:43 +0300

    feat: add Babel plugin and dependencies

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,51 +1,70 @@
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildOptions } from './types';
+import {BuildOptions} from './types';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-	const svgLoader = {
-		test: /\.svg$/,
-		use: ['@svgr/webpack'],
-	};
-	const fileLoader = {
-		test: /.(png|jpe?g|gif)$/i,
-		use: [
-			{
-				loader: 'file-loader',
-			},
-		],
-	}
-	const fontLoader = {
-		test: /\.(woff|woff2|eot|ttf|otf)$/i,
-		type: 'asset/resource',
-		generator: {
-			filename: 'assets/fonts/[name][ext]',
-		},
-	};
-	const cssLoader = {
-		test: /\.s[ac]ss$/i,
-		use: [
-			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-			{
-				loader: 'css-loader',
-				options: {
-					modules: {
-						auto: (resPath: string) =>
-							Boolean(resPath.includes('.module.')),
-						localIdentName: isDev
-							? '[path][name][ext]'
-							: '[name][ext]',
-					},
-				},
-			},
-			'sass-loader',
-		],
-	};
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
+  const fileLoader = {
+    test: /.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  }
+  const fontLoader = {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    type: 'asset/resource',
+    generator: {
+      filename: 'assets/fonts/[name][ext]',
+    },
+  };
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) =>
+              Boolean(resPath.includes('.module.')),
+            localIdentName: isDev
+              ? '[path][name][ext]'
+              : '[name][ext]',
+          },
+        },
+      },
+      'sass-loader',
+    ],
+  };
 
-	const typescriptLoader = {
-		test: /\.(tsx|ts|js)$/i,
-		use: 'ts-loader',
-		exclude: /node_modules/,
-	};
-	return [fileLoader, svgLoader, fontLoader, typescriptLoader, cssLoader];
+  const typescriptLoader = {
+    test: /\.(tsx|ts|js)$/i,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+						'i18next-extract',
+						{
+							locales: ['en', 'ru'],
+							keyAsDefaultValue: true
+						}
+					],
+        ]
+      }
+    }
+  }
+  return [fileLoader, svgLoader, fontLoader, babelLoader, typescriptLoader, cssLoader];
 }
