17a73e2151e7a9ede2fe534b4764bb56ac775483
Aliaksandr Konik <cibulyadev@gmail.com>
2023-02-08 03:22:14 +0100

    test

--- src/pages/PauseModePage/ui/PauseModePage.tsx
+++ src/pages/PauseModePage/ui/PauseModePage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import CoffeeMachineFront from '../../components/coffeeMachineFront/coffeeMachineFront';
+import CoffeeMachineFront from 'components/coffeeMachineFront/coffeeMachineFront';
 
 const PauseModePage = () => {
-  return (
-    <div>
-      <CoffeeMachineFront/>
-    </div>
-  );
+	return (
+		<div>
+			<CoffeeMachineFront />
+		</div>
+	);
 };
 
 export default PauseModePage;
