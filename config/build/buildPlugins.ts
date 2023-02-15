4aeab638e2dc346bbaac7387b7685462ac13bffe
Drobysh <austin@tut.by>
2023-02-15 16:03:00 +0300

    fix: correct import for CleanWebpackPlugin(add StylelintPlugin and ESLintPlugin as comments)

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,17 +2,21 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+import StylelintPlugin from 'stylelint-webpack-plugin';
+import ESLintPlugin from 'eslint-webpack-plugin';
 
 export function buildPlugins({
 	paths,
 	isDev
 }: BuildOptions): webpack.WebpackPluginInstance[] {
 	return [
+		//new ESLintPlugin(),
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			template: paths.html,
 		}),
+		//new StylelintPlugin(),
 		new webpack.ProgressPlugin(),
 		new MiniCssExtractPlugin({
 			filename: 'css/[name].[contenthash:8].css',
