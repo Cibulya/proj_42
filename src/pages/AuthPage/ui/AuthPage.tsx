043f24544f09a9529c319cb9978025965059cc17
Drobysh <austin@tut.by>
2023-02-17 03:06:53 +0300

    feat: add auth UI forms

--- src/pages/AuthPage/ui/AuthPage.tsx
+++ src/pages/AuthPage/ui/AuthPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SignForm from 'components/auth/SignForm';
 
 const AuthPage = () => {
   return (
-    <div>
-      
+    <div className="learning-mode">
+      <SignForm className={'card'} typeForm={'sign-up'} />
+      <SignForm className={'card'} typeForm={'sign-in'} />
     </div>
   );
 };
