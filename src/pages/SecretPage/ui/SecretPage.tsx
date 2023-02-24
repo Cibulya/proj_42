a483d17d1245bbb40c6f61a4d356b575dac371ba
Drobysh <austin@tut.by>
2023-02-24 23:24:14 +0300

    feat: add Secret and Statistic pages

---
+++ src/pages/SecretPage/ui/SecretPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SecretPage = () => {
+  return <div className="secret"></div>;
+};
+
+export default SecretPage;
--- src/providers/routeConfig/routeConfig.tsx
+++ src/providers/routeConfig/routeConfig.tsx
@@ -1,10 +1,12 @@
-import {RouteProps} from 'react-router-dom';
-import {MainPage} from 'pages/MainPage';
-import {PauseModePage} from 'pages/PauseModePage';
-import {LearningModePage} from 'pages/LearningModePage';
-import {AuthPage} from 'pages/AuthPage';
-import {AboutPage} from 'pages/AboutPage';
-import {NotFoundPage} from 'pages/NotFoundPage';
+import { RouteProps } from 'react-router-dom';
+import { MainPage } from 'pages/MainPage';
+import { PauseModePage } from 'pages/PauseModePage';
+import { LearningModePage } from 'pages/LearningModePage';
+import { AuthPage } from 'pages/AuthPage';
+import { AboutPage } from 'pages/AboutPage';
+import { SecretPage } from 'pages/SecretPage';
+import { StatisticPage } from 'pages/StatisticPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
   MAIN = 'main',
@@ -12,7 +14,9 @@ export enum AppRoutes {
   LEARNING = 'learning',
   AUTH = 'auth',
   ABOUT = 'about',
-  NOT_FOUND = 'not_found'
+  SECRET = 'secret',
+  STATISTIC = 'statistic',
+  NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
@@ -21,33 +25,42 @@ export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.LEARNING]: '/learning',
   [AppRoutes.AUTH]: '/auth',
   [AppRoutes.ABOUT]: '/about',
-  [AppRoutes.NOT_FOUND]: '*'
-}
+  [AppRoutes.SECRET]: '/secret',
+  [AppRoutes.STATISTIC]: '/statistic',
+  [AppRoutes.NOT_FOUND]: '*',
+};
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.MAIN]: {
     path: RoutePath.main,
-    element: <MainPage/>
+    element: <MainPage />,
   },
   [AppRoutes.PAUSE]: {
     path: RoutePath.pause,
-    element: <PauseModePage/>
+    element: <PauseModePage />,
   },
   [AppRoutes.LEARNING]: {
     path: RoutePath.learning,
-    element: <LearningModePage/>
+    element: <LearningModePage />,
   },
   [AppRoutes.AUTH]: {
     path: RoutePath.auth,
-    element: <AuthPage/>
+    element: <AuthPage />,
   },
   [AppRoutes.ABOUT]: {
     path: RoutePath.about,
-    element: <AboutPage/>
+    element: <AboutPage />,
+  },
+  [AppRoutes.SECRET]: {
+    path: RoutePath.secret,
+    element: <SecretPage />,
+  },
+  [AppRoutes.STATISTIC]: {
+    path: RoutePath.statistic,
+    element: <StatisticPage />,
   },
   [AppRoutes.NOT_FOUND]: {
     path: RoutePath.not_found,
-    element: <NotFoundPage/>
-  }
-
-}
+    element: <NotFoundPage />,
+  },
+};
