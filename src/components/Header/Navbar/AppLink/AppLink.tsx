d75a66855eb02f95f0da0f248d0d13a5dd6a7910
Drobysh <austin@tut.by>
2023-02-27 00:56:18 +0300

    feat: add burger menu to header

--- src/components/Header/Navbar/AppLink/AppLink.tsx
+++ src/components/Header/Navbar/AppLink/AppLink.tsx
@@ -1,33 +1,37 @@
-import React, {FC} from 'react';
-import {classNames} from 'helpers/classNames/classNames';
-import {Link, LinkProps} from 'react-router-dom';
-import { useTheme } from 'theme/useTheme';
+import React, { FC } from 'react';
+import { classNames } from 'helpers/classNames/classNames';
+import { Link, LinkProps } from 'react-router-dom';
 import { usePage } from 'theme/usePage';
 
 export enum AppLinkTheme {
   PRIMARY = 'primary',
   SECONDARY = 'secondary',
 }
-interface AppLinkProps extends LinkProps{
+interface AppLinkProps extends LinkProps {
   className?: string;
   theme?: AppLinkTheme;
+  isBurger?: boolean;
+  callback?: (value: boolean) => void;
 }
 const AppLink: FC<AppLinkProps> = (props) => {
-  const {to,
-    className,
-    theme = AppLinkTheme.PRIMARY,
-    children} = props;
+  const { to, className, theme = AppLinkTheme.PRIMARY, children, callback, isBurger } = props;
   const { page, togglePage } = usePage();
+  const linkHandler = () => {
+    if (isBurger) {
+      callback(false);
+    }
+  };
   return (
-        <Link 
-          onClick={() => {
-            togglePage(to.toString() === '/' ? 'main' : to.toString().slice(1));
-          }}
-          to={to} 
-          className={classNames('link',{}, [className, theme])}
-        >
-          {children}
-        </Link>
+    <Link
+      onClick={() => {
+        linkHandler();
+        togglePage(to.toString() === '/' ? 'main' : to.toString().slice(1));
+      }}
+      to={to}
+      className={classNames('link', {}, [className, theme])}
+    >
+      {children}
+    </Link>
   );
 };
 
